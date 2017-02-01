23c3e3171f57709eb09161026c06efcd79f18f40
William Barnes <barnesw91291@gmail.com>
2017-02-01 10:53:56 -0800

    built index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute } from 'react-router';
+import store, { history } from './store';
+import App from './containers/App';
+import Home from './components/Home';
+
+render((
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+      </Route>
+    </Router>
+  </Provider>
+), document.getElementById('app'));
